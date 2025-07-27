381f12e6623608d4db47fc544ecbb91914398f2d
Frédéric Dux <duxfrederic@gmail.com>
2025-07-27 10:47:16 -0400

    pysimplegui nuked their operation, replacing with fork

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ Created on Thu Apr 28 03:31:55 2022
 @author: fred
 """
 from time import time
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 
 from LaSillaWeatherAlerter import PhysicalMeasurement, WeatherReport,\
                                   CondensationRisk, TelescopesOpen, Alert,\
@@ -22,15 +22,15 @@ opsk = '    '.join([op for op in ops.keys()])
 text = f'Use a comparison operators:\n{opsk} , \nthen choose the limit category and' +\
         ' how long (in seconds)\nthe condition must remain true before ringing the alarm.'
 layout = [[sg.Text(text)]]
-layout += [[sg.Text(key, size=(20,1)), 
-            sg.InputText('', key=key+'cmp', size=(3,1)), 
+layout += [[sg.Text(key, size=(20,1)),
+            sg.InputText('', key=key+'cmp', size=(3,1)),
             sg.Radio('red', key, key=key+'-1'),
             sg.Radio('orange', key, key=key+'0'),
             sg.Radio('yellow', key, key=key+'1'),
             sg.Radio('green', key, key=key+'2'),
             sg.InputText('300', key=key+'duration', size=(5,1))
             ] for i,key in enumerate(list(property_lines.keys())+['Condensation Risk'])]
-layout += [[sg.Button('Set alarms', key='-setalarm-'), 
+layout += [[sg.Button('Set alarms', key='-setalarm-'),
             sg.Button('Reset alarms', key='-resetalarm-'),
             sg.Button('Exit')]]
 layout += [[sg.Text('An alarm will sound when any one of the conditions is fullfilled.')]]
@@ -47,7 +47,7 @@ try:
         if event == '-setalarm-':
             for key, item in values.items():
                 if not key.endswith('cmp'):
-                    continue 
+                    continue
                 cmp = item.strip()
                 if not cmp in ops.keys():
                     continue
@@ -66,12 +66,12 @@ try:
                     meas = PhysicalMeasurement(key, report)
                 howlong = float(values[key+'duration'])
                 alarms.append(Alert(meas, int(setlevel), cmp, howlong=howlong))
-        
+
         if event == '-resetalarm-':
             alarms = []
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
-        
+
         window['Nalarm'].update(f"{len(alarms)} alarms set.")
         # at every loop:
         infotext = []
@@ -84,7 +84,7 @@ try:
             infotext.append(text)
         window['alarmstats'].update('\n'.join(infotext))
         window.refresh()
-        
+
         for j, alarm in enumerate(alarms):
             if alarm.check():
                 text = f"Wow an alarm! {alarm.measurement.name} is at {alarm.measurement.value}"
@@ -95,7 +95,7 @@ try:
                     print(out)
                 del alarms[j]
 
-            
+
 except Exception as e:
     print(e)
 finally:
